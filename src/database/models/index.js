53cfa025c9100fd71087c30864a573f0468861cf
Bruno Home <brunoalvarado2805@gmail.com>
2021-08-17 01:27:29 -0300

    check db

--- src/database/models/index.js
+++ src/database/models/index.js
@@ -12,7 +12,16 @@ let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  //sequelize = new Sequelize(config.database, config.username, config.password, config);
+
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+        ssl: true,
+        rejectUnauthorized: false,
+    }
+  });
 }
 
 fs
